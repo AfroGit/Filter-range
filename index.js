c0ad2755b0fa9128637c68d887801da688c74285
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2022-12-13 01:58:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*  Filter range
+
+Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+The function should not modify the array. It should return the new array.*/ 
+
+ let arr = [5, 3, 8, 1];
+
+ function filterRange(arr, a, b){
+   return arr.filter(item =>(a <= item && item <= b))
+ }
+   let filtered = filterRange(arr, 1, 4);
+
+    console.log( filtered ); 
+    console.log( arr ); 
